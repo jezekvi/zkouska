ea79907bbfff132aff10a0fd3ea7ec73f53ec709
Vít Ježek <jezek.vit99@gmail.com>
2022-02-14 17:17:10 +0100

    merge_sort oprava

--- merge_sort.py
+++ merge_sort.py
@@ -18,6 +18,7 @@ def merge_sort(arr):
         a sorted list of integer numbers
     """
     if len(arr) > 1:
+        #split array into two parts
         left_arr = arr[:len(arr)//2]
         right_arr = arr[len(arr)//2:]
 
@@ -29,7 +30,8 @@ def merge_sort(arr):
         i = 0 #left_arr index
         j = 0 # right arr index
         k = 0 # merged arr index
-
+ 
+        #merging until finish one of the parts
         while i < len(left_arr) and j < len(right_arr):
             if left_arr[i] < right_arr[j]:
                 arr[k] = left_arr[i]
@@ -39,43 +41,53 @@ def merge_sort(arr):
                 j += 1
             k += 1
         
+        #append rest of the left part
         while i < len(left_arr):
             arr[k] = left_arr[i]
             i += 1
             k += 1
 
+        #append rest of the right part
         while j < len(right_arr):
             arr[k] = right_arr[j]
             j += 1
             k += 1
- 
-list = []
 
-while(True):                                                
-    n = input("Enter number of elements : ")       
-    
-    if not n.isnumeric():                               
-        print("Not number, try again")    
-        continue                                          
 
-    n = int(n)                                      
-    
-    if(n < 2):                                         
-        print("Try again and enter positive integer larger than 2")  
-    else:
-        break     
+def load_data(): 
+    list = []
 
-for i in range(0, n):
     while(True):                                                
-        ele = input()       
-    
-        if not ele.isnumeric():                               
+        n = input("Enter number of elements : ")       
+        
+        if not n.isnumeric():                               
             print("Not number, try again")    
             continue                                          
-        ele = int(ele)                                      
-        break
 
-    list.append(ele)
+        n = int(n)                                      
+        
+        if(n < 2):                                         
+            print("Try again and enter positive integer larger than 2")  
+        else:
+            break     
 
-merge_sort(list)
-print(list)
+    for i in range(0, n):
+        while(True):                                                
+            ele = input()       
+        
+            try:
+                ele = float(ele)
+                break
+            except ValueError: 
+                print("Not number, try again")    
+                                                                                                                                          
+        list.append(ele)
+    return list
+
+def program():
+    list = load_data()
+    merge_sort(list)
+    print(list)
+
+if __name__ == '__main__':
+    program()
