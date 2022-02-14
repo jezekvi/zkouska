6c72f67fd597aca345076404093633f0ce6de481
Vít Ježek <jezek.vit99@gmail.com>
2022-02-14 17:16:51 +0100

    permutace oprava

--- permutation.py
+++ permutation.py
@@ -16,21 +16,33 @@ def permutation(str, tmp=""):
     tmp : string
         temporary string used by recursion
     """
+    #end of recursion 
     if len(str) == 0:
         print(tmp)
 
+    #lock the prefix and permutate rest of the word
     for i in range(len(str)):
             ch = str[i]
             left_part = str[:i]
             right_part = str[i+1:]
             permutation(left_part + right_part, tmp + ch)
 
-while(True):                                                
-    word = input("write word: ")      
-    
-    if not re.match("^[a-zA-Z]*$", word):                              
-        print("Not a word, try again")    
-        continue                                          
-    break
 
-permutation(word)
+def load_data():
+    while(True):                                                
+        word = input("write word: ")      
+        
+        #match only if contains letters from EN alphabet
+        if not re.match("^[a-zA-Z]*$", word):                              
+            print("Not a word, try again")    
+            continue                                          
+        break
+    return word  
+
+
+def program(): 
+    word = load_data()
+    permutation(word)
+
+if __name__ == '__main__':
+    program()
